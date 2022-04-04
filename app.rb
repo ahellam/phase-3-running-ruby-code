849ffc84eab9f1d165cf40b24c092a4657ad422d
Aaron Hellam <aaronhellam@gmail.com>
2022-04-04 07:33:31 -0700

    commit tree a88e057a11395a12c3e45fe6be21ea5de25cec99

---
+++ app.rb
@@ -0,0 +1,12 @@
+# app.rb
+puts "Hello World!"
+# puts "Hello World!"
+# puts "Hello World!"
+# puts "Hello World!"
+
+p [1, 2, 3]
+
+print "Pass this test, please."
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
